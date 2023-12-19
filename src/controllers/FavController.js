82522bf54d2feedcf07dc65661bb6daf64c77bbe
iagoxz <iagomoura640@gmail.com>
2023-12-19 18:59:53 -0300

    fav update

--- src/controllers/FavController.js
+++ src/controllers/FavController.js
@@ -1,4 +1,3 @@
-// Import necessary modules and models
 const express = require("express");
 const router = express.Router();
 const mongoose = require("../database/index");
@@ -7,21 +6,17 @@ const auth = require("../middlewares/authenticate");
 const User = require("../model/User");
 const Publi = require("../model/Pub");
 
-// Route to get user's favorite publications
 router.get("/favoritos", auth, async (req, res) => {
   try {
-    // Check if the user is authenticated through the auth middleware
     if (!req.userLogged) {
       return res.status(401).send("Usuário não autenticado");
     }
 
-    // Find the user by id
     const user = await User.findOne({
       _id: req.userLogged.id,
     });
 
     if (user) {
-      // Find publications favorited by the user
       const publis = await Publi.find({
         _id: { $in: user.favoritos },
       });
@@ -34,26 +29,23 @@ router.get("/favoritos", auth, async (req, res) => {
     console.error(error);
     return res.status(500).json({
       error: "Erro ao buscar favoritos",
-      message: error.message,
+      message: error.message,   
     });
   }
 });
 
-// Route to add a publication to user's favorites
 router.put("/favoritar", auth, async (req, res) => {
   try {
     const userId = req.userLogged.id;
     const user = await User.findById(userId);
-
-    if (!user) {
+ if (!user) {
       return res.status(401).send("Usuário não autenticado");
     }
 
-    const favId = req.body.favId;
-
+    const favId = req.body.id;
+    console.log(favId, "fav")
     if (!user.favoritos.includes(favId)) {
       const publication = await Publi.findById(favId);
-
       if (!publication) {
         return res.status(404).json({ error: "Publicação não encontrada" });
       }
